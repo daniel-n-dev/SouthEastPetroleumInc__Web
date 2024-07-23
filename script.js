2a86b58034f81fbcbc7eca09b794f5a45788be34
Daniel Joshua Ngoho <danielngoho@icloud.com>
2024-07-23 18:10:16 +0800

    Applied JS
    JS added on some elements

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-$ = function(id) {
-    return document.getElementById(id);
-  }
-  
-  var show = function(id) {
-      $(id).style.display ='block';
-  }
-  var hide = function(id) {
-      $(id).style.display ='none';
-  }
+const menu = document.getElementById("menu");
+const popup = document.getElementById("menu-popup");
+
+menu.addEventListener("click", showMenu);
+
+function showMenu()
+{
+   popup.style.animation="down 1s";
+   popup.style.top="0px";
+}
+
+const button = document.getElementById("button");
+button.addEventListener("click", () =>
+{
+   popup.style.top = "-1000px";
+   popup.style.animation="up 1s";
+});
